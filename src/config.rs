f7790d25ea630cb8102a2fd8368e1876a4406650
ttdms <ttdms@vip.qq.com>
2025-03-22 08:45:56 +0800

    修改服务地址和key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["bozi.polotv.cn:21116"];
+pub const RS_PUB_KEY: &str = "oiQ+4PT2f7zQY8wX5Xu2k3ecTQAtvq4K+904YaYKMxQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
